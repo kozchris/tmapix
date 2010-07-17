b8ff9bc6314455b89a89bebafaf7c0dc33ddc00b
lars.heuer <lars.heuer@580913dc-8c39-0410-899c-37835a462aff>
2010-07-17 14:47:22 +0000

    Forgot to add XTMVersion

---
+++ tmapix-io/trunk/src/main/java/org/tmapix/io/XTMVersion.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2008 - 2010 Lars Heuer (heuer[at]semagia.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.tmapix.io;
+
+/**
+ * Provides constansts for XML Topic Maps (XTM) syntaxes.
+ * 
+ * @author Lars Heuer (heuer[at]semagia.com) <a href="http://www.semagia.com/">Semagia</a>
+ * @version $Rev:$ - $Date:$
+ */
+public enum XTMVersion {
+
+    /**
+     * Represents <a href="http://www.topicmaps.org/xtm/1.0">XTM 1.0</a>
+     */
+    XTM_1_0,
+    
+    /**
+     * Represents <a href="http://www.isotopicmaps.org/sam/sam-xtm/2006-06-19/">XTM 2.0</a>
+     */
+    XTM_2_0,
+    
+    /**
+     * Represents <a href="http://www.itscj.ipsj.or.jp/sc34/open/1378.htm">XTM 2.1</a>
+     */
+    XTM_2_1;
+}

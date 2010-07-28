54827f92323e4fb7ecd5e232e4a300fdbcc04b64
lars.heuer <lars.heuer@580913dc-8c39-0410-899c-37835a462aff>
2010-07-28 19:30:20 +0000

    Make the build work

--- tmapix-io/trunk/src/main/java/org/tmapix/io/AbstractXMLTopicMapWriter.java
+++ tmapix-io/trunk/src/main/java/org/tmapix/io/AbstractXMLTopicMapWriter.java
@@ -17,7 +17,13 @@ package org.tmapix.io;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.Iterator;
+import java.util.Set;
 
+import org.tmapi.core.Association;
+import org.tmapi.core.Role;
+import org.tmapi.core.TopicMap;
+import org.tmapix.voc.TMDM;
 import org.xml.sax.helpers.AttributesImpl;
 
 /**
@@ -34,6 +40,14 @@ abstract class AbstractXMLTopicMapWriter extends AbstractTopicMapWriter {
     protected final AttributesImpl _attrs;
 
     protected final XMLWriter _out;
+
+    private Object _typeInstance;
+
+    private Object _type;
+
+    private Object _instance;
+
+    private boolean _checkForTypeInstanceAssociations;
 
     /**
      * Creates a new instance using "utf-8" encoding.
@@ -97,6 +111,40 @@ abstract class AbstractXMLTopicMapWriter extends AbstractTopicMapWriter {
      */
     protected void addAttribute(final String name, final String value) {
         _attrs.addAttribute("", name, "", "CDATA", value);
+    }
+
+    protected void init(final TopicMap topicMap) {
+        _typeInstance = topicMap.getTopicBySubjectIdentifier(topicMap.createLocator(TMDM.TYPE_INSTANCE));
+        _type = topicMap.getTopicBySubjectIdentifier(topicMap.createLocator(TMDM.TYPE));
+        _instance = topicMap.getTopicBySubjectIdentifier(topicMap.createLocator(TMDM.INSTANCE));
+        _checkForTypeInstanceAssociations = _typeInstance != null && _type != null && _instance != null;
+    }
+
+    /**
+     * Returns if the provided association represents a type-instance relationship.
+     *
+     * @param assoc The association.
+     * @param roles The roles of the association.
+     * @return {@code true} if the association represents a type-instance relationship,
+     *          otherwise {@code false}.
+     */
+    protected final boolean isTypeInstanceAssociation(final Association assoc, 
+            final Set<Role> roles) {
+        if (!_checkForTypeInstanceAssociations 
+                || !assoc.getType().equals(_typeInstance)
+                || assoc.getReifier() != null
+                || !assoc.getScope().isEmpty()
+                || roles.size() != 2) {
+            return false;
+        }
+        final Iterator<Role> roleIter = roles.iterator();
+        final Role firstRole = roleIter.next();
+        final Role secondRole = roleIter.next();
+        if (firstRole.getType().equals(_type)) {
+            return secondRole.getType().equals(_instance);
+        }
+        return secondRole.getType().equals(_instance) 
+                    && firstRole.getType().equals(_type);
     }
 
 }

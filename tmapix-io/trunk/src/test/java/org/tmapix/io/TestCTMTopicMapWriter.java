88c471ee8564d71406e40834f9978c35176a798e
lars.heuer <lars.heuer@580913dc-8c39-0410-899c-37835a462aff>
2010-08-02 19:21:14 +0000

    Ported tinyTiM's CTM writer to TMAPIX

---
+++ tmapix-io/trunk/src/test/java/org/tmapix/io/TestCTMTopicMapWriter.java
@@ -0,0 +1,107 @@
+/*
+ * Copyright 2008 - 2010 Lars Heuer (heuer[at]semagia.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.tmapix.io;
+
+import java.io.File;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.tmapix.io.CXTMTestUtils.Filter;
+
+/**
+ * Tests against the {@link CTMTopicMapWriter}.
+ * 
+ * @author Lars Heuer (heuer[at]semagia.com) <a href="http://www.semagia.com/">Semagia</a>
+ * @version $Rev$ - $Date$
+ */
+@RunWith(Parameterized.class)
+public class TestCTMTopicMapWriter extends AbstractCXTMWriterTestCase {
+
+    public TestCTMTopicMapWriter(File file, String inputDir,
+            String referenceDir, boolean convertToTMDM) {
+        super(file, inputDir, referenceDir, convertToTMDM);
+    }
+
+    @Parameters
+    public static Collection<Object> makeTestCases() {
+        final Collection<Object> result = new ArrayList<Object>(); 
+        result.addAll(Filter.from("/cxtm/xtm2/", "/cxtm/xtm21/")
+                .using("xtm")
+                .exclude("subjid-escaping.xtm",
+                        // Constructs != topic which have an iid
+                        "association-duplicate-iid.xtm",
+                        "association-duplicate-reified2.xtm",
+                        "association-duplicate-reified3.xtm",
+                        "association-duplicate-reified4.xtm",
+                        "itemid-association.xtm",
+                        "itemid-name.xtm",
+                        "itemid-occurrence.xtm",
+                        "itemid-role.xtm",
+                        "itemid-tm.xtm",
+                        "itemid-variant.xtm",
+                        "mergemap-itemid.xtm",
+                        "name-duplicate-iid.xtm",
+                        "name-duplicate-reified3.xtm",
+                        "name-duplicate-reified4.xtm",
+                        "occurrence-duplicate-iid.xtm",
+                        "occurrence-duplicate-iid2.xtm",
+                        "role-duplicate-iid.xtm",
+                        "role-duplicate-iid2.xtm",
+                        // Unsure about the following, parser fails
+                        "role-duplicate-reified.xtm",
+                        "role-duplicate-reified2.xtm",
+                        "role-duplicate-reified3.xtm",
+                        "role-duplicate-reified4.xtm",
+                        "role-reifier.xtm",
+                        "role-reifier-element-sid.xtm",
+                        "role-reifier-element-slo.xtm",
+                        "role-reifier-element-topicref.xtm",
+                        // End of unsure
+                        "variant-duplicate-iid.xtm",
+                        "topicref-no-fragment-id.xtm"   // unsure
+                        ).filter());
+        result.addAll(Filter.from("/cxtm/tmxml/")
+                .using("xml")
+                .exclude("occurrence-datetime.xml" // Parser fails :(
+                        ).filter());
+        result.addAll(CXTMTestUtils.makeSnelloTestCases());
+        return result;
+    }
+    
+    /* (non-Javadoc)
+     * @see org.tmapix.io.AbstractCXTMWriterTestCase#makeWriter(org.tmapi.core.TopicMap, java.lang.String)
+     */
+    @Override
+    protected TopicMapWriter makeWriter(final OutputStream out, String iri)
+            throws Exception {
+        final CTMTopicMapWriter writer = new CTMTopicMapWriter(out, iri);
+        writer.setExportItemIdentifiers(true);
+        return writer;
+    }
+
+    /* (non-Javadoc)
+     * @see org.tmapix.io.AbstractCXTMWriterTestCase#getFileExtension()
+     */
+    @Override
+    protected String getFileExtension() {
+        return "ctm";
+    }
+
+}
